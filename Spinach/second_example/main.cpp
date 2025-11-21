38e18dc491a159741003025078bd092062ab6ca0
Manoj M J <mmjthefighter@gmail.com>
2025-11-21 21:51:46 +0530

    Add svg rendering example

--- Spinach/second_example/main.cpp
+++ Spinach/second_example/main.cpp
@@ -12,10 +12,27 @@ int main(int argc, char* argv[])
 	spn::Image* sepiaImage = img.Clone();
 	ApplySepiaFilter(sepiaImage);
 
+	spn::Image svgImg;
+	svgImg.CreateFromSvg("res/NAND_ANSI.svg",100);
+
 	spn::SpinachCore sc(640, 480);
 
 	if (sc.IsInitSucceded()){
 		spn::Canvas* canvas = sc.GetCanvas();
+
+		canvas->SetClearColor(255, 255, 255);
+		canvas->Clear();
+		canvas->SetPrimaryColor(0, 0, 128);
+		canvas->EnableAlphaBlending(true);//needed for rendering images with transparency whether loaded from png or svg.
+		canvas->DrawImage(&svgImg, 10, 10);
+		canvas->DrawText("NAN gate rendered from svg", 100, 150);
+		sc.RenderCanvas();
+
+		sc.WaitForEvents();
+		canvas->EnableAlphaBlending(false);//we are not dealing with images with transparency anymore, this is important because bitblt calls will be fast.
+		canvas->SetClearColor(0, 0, 0);
+
+		canvas->Clear();
 		canvas->SetPrimaryColor(0, 255, 0);
 		canvas->DrawImage(&img,0,0);
 		canvas->DrawText("base image", 100, 400);
