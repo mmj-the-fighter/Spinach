e9967b6e6447d782658374ccee28f1aaa333bcf9
Manoj M J <mmjthefighter@gmail.com>
2026-02-22 08:51:24 +0530

    Remove unwanted code

--- Spinach/examples/raytracing_example/main.cpp
+++ Spinach/examples/raytracing_example/main.cpp
@@ -12,45 +12,16 @@ spn::SpinachCore* pSc = nullptr;
 bool userWantsToQuit = false;
 
 
-void CopyPixelsToCanvas(spn::Canvas* canvas, int scanline) {
-	int height = canvas->GetHeight();
-	int width = canvas->GetWidth();
-	unsigned char* pixBuffer = canvas->GetPixelBuffer();
-	int pitch = canvas->GetPitch();
-	int channels = 4;
-	for (int y = 0; y < scanline; ++y)
-	{
-		for (int x = 0; x < width; ++x)
-		{
-			unsigned char* aLoc = pixBuffer + pitch * y + x * channels;
-			*aLoc = 128;
-			*(aLoc + 1) = 0;
-			*(aLoc + 2) = 0;
-			*(aLoc + 3) = 255;
-		}
-	}
-}
-
 void RayTracerThreadFn() {
 	rt->Render();
 	std::cout << "Rendering completed";
 }
 
-
-void InputHandler(const SDL_Event* event) {
-	/*if (event->type == SDL_EVENT_KEY_UP ||
-		event->type == SDL_EVENT_QUIT ||
-		event->type == SDL_EVENT_MOUSE_BUTTON_UP
-		) {
-		rt->SetUserWantsToQuit(true);
-	}*/
-}
-
-
 int main(int argc, char* argv[])
 {
 	int imageWt = 640;
-	int imageHt = RayTracer::CalculateImageHeight(16.0 / 9.0, 640);
+	double aspectRatio = 16.0/9.0;
+	int imageHt = RayTracer::CalculateImageHeight(aspectRatio, imageWt);
 	spn::SpinachCore sc(imageWt, imageHt, "../res/"); //Note: 3rd argument is path rel. to build folder
 	if (sc.IsInitFailed()) {
 		std::cout << "initialization failed with error "
@@ -62,7 +33,6 @@ int main(int argc, char* argv[])
 	sc.GetCanvas()->SetPrimaryColor(255, 255, 0);
 	sc.GetCanvas()->SetClearColor(0, 0, 0);
 	sc.GetCanvas()->Clear();
-	sc.SetInputHandler(InputHandler);
 	pSc = &sc;
 	rt = std::make_unique<RayTracer>();
 	rt->Init(sc.GetCanvas(), 1.0, 2.0, point3(0,0,0));
